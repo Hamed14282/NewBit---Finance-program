7cf6b6b44c0922e0a6368c082342014b6ffdebfb
TFHHA <hosseinihamed220@gmail.com>
2026-04-29 15:07:09 +0200

    Simple interest GUI done

--- GUItest.py
+++ GUItest.py
@@ -15,7 +15,7 @@ window.grid_rowconfigure(1, weight=1)
 ###############################################################################################################
 
 frame1 = customtkinter.CTkFrame(master=window)
-frame1.grid(row=0, column=0, pady=10, padx=10, sticky="nsw")
+frame1.grid(row=0, column=0, pady=15, padx=15, sticky="nsw")
 frame1.grid_columnconfigure(0, weight=0)
 frame1.grid_rowconfigure(0, weight=0)
 frame1.grid_rowconfigure(1, weight=0)
@@ -24,20 +24,21 @@ frame1.grid_rowconfigure(2, weight=0)
 
 frame2 = None
 frame3 = None
+frame6 = None
 
 def create_frame2():
     global frame2
     frame2 = customtkinter.CTkFrame(master=window)
-    frame2.grid(row=0, column=1, pady=10, padx=10, sticky="nse")
+    frame2.grid(row=0, column=1, pady=15, padx=15, sticky="nse")
     frame2.grid_columnconfigure(0, weight=1)
     frame2.grid_columnconfigure(1, weight=1)
 
 def create_frame2(x, y):
     global frame2
     frame2 = customtkinter.CTkFrame(master=window)
-    frame2.grid(row=y, column=x, pady=10, padx=10, sticky="nsew")
+    frame2.grid(row=y, column=x, pady=15, padx=15, sticky="nsew")
     if x == 0 and y == 1:
-        frame2.grid(row=y, column=x, pady=10, padx=10, sticky="nsw")
+        frame2.grid(row=y, column=x, pady=15, padx=15, sticky="nsw")
         frame2.grid_columnconfigure(0, weight=0)
         frame2.grid_rowconfigure(0, weight=0)
     frame2.grid_columnconfigure(0, weight=1)
@@ -46,14 +47,14 @@ def create_frame2(x, y):
 def create_frame3():
     global frame3
     frame3 = customtkinter.CTkFrame(master=window)
-    frame3.grid(row=1, column=1, pady=10, padx=10, sticky="nsew")
+    frame3.grid(row=1, column=1, pady=15, padx=15, sticky="nsew")
     frame3.grid_columnconfigure(0, weight=1)
     frame3.grid_columnconfigure(1, weight=1)
 
 def create_frame3(x, y):
     global frame3
     frame3 = customtkinter.CTkFrame(master=window)
-    frame3.grid(row=y, column=x, pady=10, padx=10, sticky="nsew")
+    frame3.grid(row=y, column=x, pady=15, padx=15, sticky="nsew")
     frame3.grid_columnconfigure(0, weight=1)
     frame3.grid_columnconfigure(1, weight=1)
 
@@ -163,30 +164,37 @@ def select(case):
                 match choice:
                     case "Simple Interest":
                         create_frame3(1, 0)
+                        
+                        frame4 = customtkinter.CTkFrame(master=frame3, fg_color="transparent", bg_color="transparent")
+                        frame4.grid(row=0, column=0, pady=0, padx=0)
 
-                        label4 = customtkinter.CTkLabel(master=frame3, text="Simple interest: Total savings after projected years with simple interest", font=("Roboto", 24))
-                        label4.grid(row=1, column=1, pady=10, padx=10)
+                        frame5 = customtkinter.CTkFrame(master=frame3, fg_color="transparent", bg_color="transparent")
+                        frame5.grid(row=1, column=0, pady=0, padx=0)
 
-                        label5 = customtkinter.CTkLabel(master=frame3, text=f"Savings: €{Finance.savings:.2f}", font=("Roboto", 16))
-                        label5.grid(row=2, column=1, pady=10, padx=10)
 
-                        label6 = customtkinter.CTkLabel(master=frame3, text=f"Portion of savings affected by interest", font=("Roboto", 16))
-                        label6.grid(row=3, column=0, pady=10, padx=10)
+                        label4 = customtkinter.CTkLabel(master=frame4, text="Simple interest: Total savings after projected years with simple interest", font=("Roboto", 24))
+                        label4.grid(row=0, column=0, pady=10, padx=10)
 
-                        entry1 = customtkinter.CTkEntry(master=frame3, placeholder_text="Enter portion of savings affected by interest")
-                        entry1.grid(row=3, column=1, pady=10, padx=10)
+                        label5 = customtkinter.CTkLabel(master=frame4, text=f"Savings: €{Finance.savings:.2f}", font=("Roboto", 16))
+                        label5.grid(row=1, column=0, pady=10, padx=10)
 
-                        label7 = customtkinter.CTkLabel(master=frame3, text="Annual interest rate(percentage):", font=("Roboto", 16))
-                        label7.grid(row=4, column=0, pady=10, padx=10)
+                        label6 = customtkinter.CTkLabel(master=frame5, text="Portion of savings affected by interest:", font=("Roboto", 16))
+                        label6.grid(row=0, column=0, pady=10, padx=10)
 
-                        entry2 = customtkinter.CTkEntry(master=frame3, placeholder_text="Enter annual interest rate")
-                        entry2.grid(row=4, column=1, pady=10, padx=10)
+                        entry1 = customtkinter.CTkEntry(master=frame5, placeholder_text="Enter portion of savings affected by interest")
+                        entry1.grid(row=0, column=1, pady=10, padx=10)
 
-                        label8 = customtkinter.CTkLabel(master=frame3, text="Projected months:", font=("Roboto", 16))
-                        label8.grid(row=5, column=0, pady=10, padx=10)
+                        label7 = customtkinter.CTkLabel(master=frame5, text="Annual interest rate(percentage):", font=("Roboto", 16))
+                        label7.grid(row=1, column=0, pady=10, padx=10)
 
-                        entry3 = customtkinter.CTkEntry(master=frame3, placeholder_text="Enter projected months")
-                        entry3.grid(row=5, column=1, pady=10, padx=10)
+                        entry2 = customtkinter.CTkEntry(master=frame5, placeholder_text="Enter annual interest rate")
+                        entry2.grid(row=1, column=1, pady=10, padx=10)
+
+                        label8 = customtkinter.CTkLabel(master=frame5, text="Projected months:", font=("Roboto", 16))
+                        label8.grid(row=2, column=0, pady=10, padx=10)
+
+                        entry3 = customtkinter.CTkEntry(master=frame5, placeholder_text="Enter projected months")
+                        entry3.grid(row=2, column=1, pady=10, padx=10)
 
 
                         def calculate_simple_interest():
@@ -195,19 +203,26 @@ def select(case):
                             years = int(entry3.get())/12
                             result = Finance.simple_interest(annual_rate, years, interest_money)
 
-                            for widget in frame3.grid_slaves(row=6, column=1):
-                                widget.destroy()  # Clear previous result if exists
-
+                            global frame6
+                            if frame6 is None or not frame6.winfo_exists():
+                                frame6 = customtkinter.CTkFrame(master=frame3, fg_color="transparent", bg_color="transparent")
+                                frame6.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
+                            
+                            # Clear any existing frame at this position
+                            for widget in frame6.grid_slaves(row=0, column=0):
+                                widget.destroy()
+                            
                             if interest_money > Finance.savings or interest_money <= 0:
-                                label9 = customtkinter.CTkLabel(master=frame3, text="Invalid portion of savings affected by interest. Please enter a value between 0 and " + str(Finance.savings), font=("Roboto", 16))
-                                label9.grid(row=6, column=1, pady=10, padx=10)
+                                label10 = customtkinter.CTkLabel(master=frame6, text="Invalid portion of savings affected by interest. Please enter a value between 0 and " + str(Finance.savings), font=("Roboto", 16))
+                                label10.grid(row=0, column=0, pady=10, padx=10)
                             else:
-                                label9 = customtkinter.CTkLabel(master=frame3, text="Total savings of €" + str(result) + " after " + str(years) + " years with simple interest", font=("Roboto", 16))
-                                label9.grid(row=6, column=1, pady=10, padx=10)
+                                label9 = customtkinter.CTkLabel(master=frame6, text="Total savings of €" + str(result) + " after " + str(years) + " years with simple interest", font=("Roboto", 16))
+                                label9.grid(row=0, column=0, pady=10, padx=10)
 
-                        button4 = customtkinter.CTkButton(master=frame3, text="Calculate", command=calculate_simple_interest)
-                        button4.grid(row=5, column=2, pady=10, padx=10)
+                        button4 = customtkinter.CTkButton(master=frame5, text="Calculate", command=calculate_simple_interest)
+                        button4.grid(row=2, column=2, pady=10, padx=10)
 
+#NEEDS WORK##################################################################################################
                     case "Compound Interest":
                         create_frame3(1, 0)
 
@@ -217,7 +232,7 @@ def select(case):
                         label5 = customtkinter.CTkLabel(master=frame3, text=f"Savings: €{Finance.savings:.2f}", font=("Roboto", 16))
                         label5.grid(row=2, column=1, pady=10, padx=10)
                         
-                        label6 = customtkinter.CTkLabel(master=frame3, text=f"Portion of savings affected by interest", font=("Roboto", 16))
+                        label6 = customtkinter.CTkLabel(master=frame3, text="Portion of savings affected by interest:", font=("Roboto", 16))
                         label6.grid(row=3, column=0, pady=10, padx=10)
                          
                         entry1 = customtkinter.CTkEntry(master=frame3, placeholder_text="Enter portion of savings affected by interest")
